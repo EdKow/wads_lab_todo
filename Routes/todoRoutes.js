00a6b7a699d26408dcefee9f085f0a768220ade6
Edkow <kowandaedward@gmail.com>
2025-03-21 00:38:26 +0700

    heh

--- Routes/todoRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createTodo } from "../controllers/todolist.js";
-
-const router = express.Router()
-
-router.post("/add_todo", createTodo)
-
-export default router
